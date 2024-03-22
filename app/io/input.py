89a35b4e8caa11a7747f25c3dc6379793fa2ff32
Oleksiy Zhytnetskyi <o.zhytnetskyi@ukma.edu.ua>
2024-03-22 16:55:02 +0200

    Implement input and output function signatures

---
+++ app/io/input.py
@@ -0,0 +1,10 @@
+def from_console():
+    pass
+
+
+def from_file(file_path):
+    pass
+
+
+def from_file_pd(file_path):
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def to_console(text):
+    pass
+
+
+def to_file(file_path, text):
+    pass
+
+
+def to_file_pd(file_path, text):
+    pass
